963e23ed686efbddf65989e51d7b1e799077c21f
arrrrrmin <43553423+arrrrrmin@users.noreply.github.com>
2025-11-05 13:29:21 +0100

    ➕ Added

--- scenarios/vite.config.js
+++ scenarios/vite.config.js
@@ -1,10 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { visualizer } from 'rollup-plugin-visualizer';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
 	plugins: [
 		tailwindcss(),
 		sveltekit(),
+		visualizer({
+			emitFile: true,
+			filename: 'stats.html'
+		})
 	],
 });
